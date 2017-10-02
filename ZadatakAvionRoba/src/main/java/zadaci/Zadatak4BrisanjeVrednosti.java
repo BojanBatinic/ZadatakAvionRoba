11977d316499188ef4a601f5d45dacaf813f961b
BojanBatinic <bojanbatinic77@gmail.com>
2017-10-02 20:33:54 +0200

    Uradjen zadatak brisanja vrednosti, fixes #4

---
+++ ZadatakAvionRoba/src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,55 @@
+package zadaci;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import model.Roba;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Zadatak4BrisanjeVrednosti {
+
+    static Dao<Roba,Integer> robaDao;
+
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+
+            connectionSource = new JdbcConnectionSource(Konstante.DATABASE_URL);
+
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+            List<Roba> robe = robaDao.queryForAll();
+            System.out.println("Prikaz sve robe da " +
+                    "proverimo da je  " +
+                    "sva roba uneta.");
+            for(Roba r : robe)
+                System.out.println(r);
+
+            List<Roba> zaBrisanje = robaDao.queryForEq(Roba.POLJE_NAZIV,"Voda");
+            robaDao.delete(zaBrisanje);
+
+
+            robe = robaDao.queryForAll();
+            System.out.println("Prikaz sve robe da " +
+                    "proverimo da je  " +
+                    "sva roba uneta.");
+            for(Roba r : robe)
+                System.out.println(r);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
