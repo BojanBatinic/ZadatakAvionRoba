a6f24bdfc028b96611ed7c7f8addfda28768a5a4
BojanBatinic <bojanbatinic77@gmail.com>
2017-10-02 20:27:41 +0200

    Uradjen zadatak izmena vrednosti, fixes #3

---
+++ ZadatakAvionRoba/src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -0,0 +1,56 @@
+package zadaci;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Avion;
+import model.Roba;
+import java.util.List;
+import java.io.IOException;
+
+public class Zadatak3IzmenaVrednosti {
+
+    static Dao<Roba,Integer> robaDao;
+
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+
+            connectionSource = new JdbcConnectionSource(Konstante.DATABASE_URL);
+
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+            List<Roba> robe = robaDao.queryForAll();
+            System.out.println("Prikaz sve robe da " +
+                    "proverimo da je  " +
+                    "sva roba uneta.");
+            for(Roba r : robe)
+                System.out.println(r);
+
+            List<Roba> zaIzmenu = robaDao.queryForEq(Roba.POLJE_OPIS,"Plasticna stolica");
+            zaIzmenu.get(0).setOpis("Drvena stolica");
+            robaDao.update(zaIzmenu.get(0));
+
+            robe = robaDao.queryForAll();
+            System.out.println("Prikaz sve robe da " +
+                    "proverimo da je  " +
+                    "sva roba uneta.");
+            for(Roba r : robe)
+                System.out.println(r);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
