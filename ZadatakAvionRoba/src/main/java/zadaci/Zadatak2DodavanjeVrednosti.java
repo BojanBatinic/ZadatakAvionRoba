383c0f7f6d7f158ac61339ea3de72516dee9c43d
Bojan <new@mail.com>
2017-10-05 14:52:54 +0200

    Uradjen zadatak brisanja vrednosti, fixes #6

--- ZadatakAvionRoba/src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
+++ ZadatakAvionRoba/src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
@@ -22,8 +22,9 @@ public class Zadatak2DodavanjeVrednosti {
 
             connectionSource = new JdbcConnectionSource(Konstante.DATABASE_URL);
 
-            robaDao = DaoManager.createDao(connectionSource, Roba.class);
             avionDao = DaoManager.createDao(connectionSource, Avion.class);
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
 
             TableUtils.clearTable(connectionSource,Avion.class);
             TableUtils.clearTable(connectionSource,Roba.class);
@@ -34,19 +35,24 @@ public class Zadatak2DodavanjeVrednosti {
             Avion a2 = new Avion("Avion2", 21);
             avionDao.create(a2);
 
-            Roba r1 = new Roba("Patike","Duboke patike", 1, a1);
+            Roba r1 = new Roba("Patike","Duboke patike", 1);
+            r1.setAvion(a1);
             robaDao.create(r1);
 
-            Roba r2 = new Roba("Kosulja","Na duge rukave", 0.4, a1);
+            Roba r2 = new Roba("Kosulja","Na duge rukave", 0.4);
+            r2.setAvion(a1);
             robaDao.create(r2);
 
-            Roba r3 = new Roba("Voda","Voda za pice", 1.4, a1);
+            Roba r3 = new Roba("Voda","Voda za pice", 1.4);
+            r3.setAvion(a1);
             robaDao.create(r3);
 
-            Roba r4 = new Roba("Ploce","Drvene ploce", 3.4, a2);
+            Roba r4 = new Roba("Ploce","Drvene ploce", 3.4);
+            r4.setAvion(a2);
             robaDao.create(r4);
 
-            Roba r5 = new Roba("Stolica","Plasticna stolica", 2.4, a2);
+            Roba r5 = new Roba("Stolica","Plasticna stolica", 2.4);
+            r5.setAvion(a2);
             robaDao.create(r5);
 
             List<Roba> robe = robaDao.queryForAll();
